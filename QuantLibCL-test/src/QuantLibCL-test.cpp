9defb2831378083667d764942720f8d8a168626e
wgross1172 <wgross1172@4146dba0-0ca5-11df-a5af-b38b39d06e06>
2010-01-30 10:42:15 +0000

    Created initial QuantLibCL-test project.

---
+++ QuantLibCL-test/src/QuantLibCL-test.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : QuantLibCL-test.cpp
+ Author      : William Gross
+ Version     :
+ Copyright   : 
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World!!!" << endl; /* prints Hello World!!! */
+	return 0;
+}
