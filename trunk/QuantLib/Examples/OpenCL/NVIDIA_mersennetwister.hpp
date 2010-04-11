4317025da784fadcbab470deb5a6cb6453280395
wgross1172 <wgross1172@4146dba0-0ca5-11df-a5af-b38b39d06e06>
2010-04-11 04:59:24 +0000

    Minor additions to OpenCL example

--- trunk/QuantLib/Examples/OpenCL/NVIDIA_mersennetwister.hpp
+++ trunk/QuantLib/Examples/OpenCL/NVIDIA_mersennetwister.hpp
@@ -5,7 +5,7 @@
 #define NVIDIA_MT_RNG_COUNT 4096
 
 // # of rvs that each thread will generate
-#define NVIDIA_RVs_PER_THREAD 512
+#define NVIDIA_RVs_PER_THREAD 1024
 
 // Total # of rvs that will be generated
 #define NVIDIA_nRand (NVIDIA_MT_RNG_COUNT * NVIDIA_RVs_PER_THREAD)
